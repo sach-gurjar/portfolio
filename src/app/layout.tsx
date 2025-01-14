fa29c3a4327a3bcb469ff2b765a68f50dcc3dc1d
sach-gurjar <gurjarsachin34@gmail.com>
2025-01-14 18:14:51 +0530

    add meta tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -24,22 +24,40 @@ export default function RootLayout({
       offset: 200, // Offset from the element (in px)
       easing: "ease-in-out", // Easing function
       // delay: 100, // Delay before animation starts (in ms)
-     
     });
   }, []);
   return (
     <html suppressHydrationWarning lang="en">
-      {/*
-        <head /> will contain the components returned by the nearest parent
-        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
-      */}
-      <head />
+      <head>
+        <title>Sachin Gurjar - Portfolio</title>
+        <meta
+          name="description"
+          content="Welcome to the portfolio of Sachin Gurjar, a passionate software developer creating innovative solutions. Discover my projects and skills."
+        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <meta name="author" content="Sachin Gurjar" />
+        <meta
+          name="keywords"
+          content="Sachin Gurjar, portfolio, software developer, projects, coding"
+        />
+        <meta property="og:title" content="Sachin Gurjar - Portfolio" />
+        <meta
+          property="og:description"
+          content="Explore the portfolio of Sachin Gurjar, showcasing innovative software development projects and skills."
+        />
+        <meta property="og:image" content="https://sachingurjar.netlify.app/_next/image?url=%2Fimages%2Fsachin.HEIC&w=750&q=75" />
+        <meta property="og:url" content="https://sachingurjar.netlify.app/" />
+        <meta name="twitter:card" content="summary_large_image" />
+        
+        {/* Theme Meta Tag */}
+        <meta name="theme-color" content="#FFFFFF" media="(prefers-color-scheme: light)" />
+        <meta name="theme-color" content="#000000" media="(prefers-color-scheme: dark)" />
+      </head>
 
       <body className={`bg-[#FCFCFC] dark:bg-black ${inter.className}`}>
         <Providers>
           <Header />
           {children}
-
           <ScrollToTop />
         </Providers>
       </body>

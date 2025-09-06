184b8aaf7151bfebebc8f74824a8a0e6ecc6edd1
sachin gurjar <67531447+sach-gurjar@users.noreply.github.com>
2025-09-06 16:44:31 +0530

    fix netlify build issue

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // output: "export", // Enables static export
+  output: "export", // Enables static export
   images: {
     remotePatterns: [
       {
